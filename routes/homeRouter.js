0c78d1fb8e292bdf73d20dde34f2c7478a1d9620
gnomgad <canimore@yandex.ua>
2021-08-02 11:23:09 +0300

    chore: настроил роут

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const homeController = require("../controllers/homeControllers.js");
+const homeController = require("../controllers/homeControllers");
+const covController = require("../controllers/convertControllers");
 
 const homeRouter = express.Router();
+
+homeRouter.get("/1-OPT", covController.convertOneOpt);
+homeRouter.get("/1-PE", covController.convertOnePE);
 
 homeRouter.get("/", homeController.index);
 
